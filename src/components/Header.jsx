f17db783763119617207bdbcfca7533446bb0491
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-11-18 17:22:47 +0100

    Create Navbar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,9 @@
-import { NavLink } from "react-router-dom";
+import Navbar from "./Navbar";
 
 export default function Header() {
     return (
         <header>
-            <nav className="navbar navbar-expand navbar-light bg-light shadow-sm">
-                <div className="nav navbar-nav d-flex gap-5 mx-auto">
-                    <NavLink className="nav-item nav-link" to="/">Home</NavLink>
-                    <NavLink className="nav-item nav-link" to="/about">Chi siamo</NavLink>
-                    <NavLink className="nav-item nav-link" to="/products">Prodotti</NavLink>
-                </div>
-            </nav>
+            <Navbar />
         </header>
     );
 }

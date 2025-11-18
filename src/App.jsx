3b681b5e0f07abd01ef67f00ef7b6d6aff105373
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-11-18 17:08:34 +0100

    add BudgetContext and integrate budgetMode state in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useState } from "react"
+import BudgetContext from "./contexts/BudgetContext"
 import DefaultLeyout from "./layouts/DefaultLayout"
 import HomePage from "./pages/HomePage"
 import AboutUs from "./pages/AboutUs"
@@ -6,19 +8,23 @@ import Products from "./pages/Products"
 import Product from "./pages/Product"
 
 function App() {
+  const [budgetMode, setBudgetMode] = useState(false)
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLeyout />} >
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutUs />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/products/:id" element={<Product />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+
+      <BudgetContext.Provider value={{ budgetMode, setBudgetMode }}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLeyout />} >
+              <Route path="/" element={<HomePage />} />
+              <Route path="/about" element={<AboutUs />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/products/:id" element={<Product />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </BudgetContext.Provider>
     </>
   )
 }
